10d37ee1d51a4b7dfcad08f38b223add11064582
jeun66 <sje040626@naver.com>
2025-07-02 22:37:10 +0900

    JavaScript - fetch

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -1,4 +1,15 @@
-const items={
-    {},
-    {},
+//Fetch the items from the JSON file
+function loadItems(){
+    return fetch('data/data.json') //fetch로 데이터 받아옴
+    .then(response =>response.json()) //데이터 받아오기 성공시(배열형식 쭉 이어진 문자열로 받아옴, 읽을 수 없음), js으로 변환(데이터 읽을 수 있음) :데이터 파싱
+    .then(json=>json.items); //json안의 데이터 받아옴
 }
+
+//main
+loadItems()
+.then(items => {
+    console.log(items);
+   // displayItems(item);
+   // setEventListeners(itmes)
+})
+.catch(console.log)
